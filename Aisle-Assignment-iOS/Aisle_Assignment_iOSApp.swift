283aa5ef34081e70e155a092497aa86ae090109b
arifashraf4 <arifashraf4@gmail.com>
2023-02-15 16:25:59 +0530

    Initial Commit

---
+++ Aisle-Assignment-iOS/Aisle_Assignment_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aisle_Assignment_iOSApp.swift
+//  Aisle-Assignment-iOS
+//
+//  Created by arifashraf on 15/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Aisle_Assignment_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
